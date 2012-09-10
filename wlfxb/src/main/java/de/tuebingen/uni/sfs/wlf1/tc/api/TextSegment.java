23dc32bb8951c30d740b7bb2582925809cb136bf
meh <marie.hinrichs@uni-tuebingen.de>
2012-09-10 11:29:55 +0000

    moved one level up

--- wlfxb/src/main/java/de/tuebingen/uni/sfs/wlf1/tc/api/TextSegment.java
+++
@@ -1,7 +0,0 @@
-package de.tuebingen.uni.sfs.wlf1.tc.api;
-
-public interface TextSegment {
-
-    public String getText();
-    //public void setText();
-}
--- wlfxb/src/test/java/de/tuebingen/uni/sfs/jaxbtest/TokensLayerI.java
+++
@@ -1,8 +0,0 @@
-package de.tuebingen.uni.sfs.jaxbtest;
-
-import java.util.List;
-
-public interface TokensLayerI {
-
-    public List<TokenI> getTokens();
-}
