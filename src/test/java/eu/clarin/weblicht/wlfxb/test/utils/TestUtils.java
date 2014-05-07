9dcda75f7a0ad6af3cf7368bd5a93c3ddbf31c0c
Yana Panchenko <yana.panchenko@uni-tuebingen.de>
2014-05-07 18:01:51 +0200

    change xml comparison in tests to use xmlunit
    add xmlunit dependency in test scope
    compare xml so that namespaces' prefix names do not affect the comparison
    change a few expected xml to have different prefix names than really expected, this should not fail the tests

--- src/test/java/eu/clarin/weblicht/wlfxb/test/utils/TestUtils.java
+++ src/test/java/eu/clarin/weblicht/wlfxb/test/utils/TestUtils.java
@@ -6,6 +6,7 @@ package eu.clarin.weblicht.wlfxb.test.utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -13,6 +14,11 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.Difference;
+import org.custommonkey.xmlunit.DifferenceEngine;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Assert;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -28,18 +34,53 @@ public class TestUtils {
      * @param outputFile
      * @throws Exception
      */
+//    public static void assertEqualXml(InputStream expectedFile, InputStream realFile) throws ParserConfigurationException, SAXException, IOException {
+//        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//        dbf.setNamespaceAware(true);
+//        dbf.setCoalescing(true);
+//        dbf.setIgnoringElementContentWhitespace(true);
+//        dbf.setIgnoringComments(true);
+//        DocumentBuilder db = dbf.newDocumentBuilder();
+//        Document doc1 = db.parse(expectedFile);
+//        doc1.normalizeDocument();
+//        Document doc2 = db.parse(realFile);
+//        doc2.normalizeDocument();
+//        Assert.assertTrue(doc1.isEqualNode(doc2));
+//    }
+//    
     public static void assertEqualXml(InputStream expectedFile, InputStream realFile) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
-        dbf.setCoalescing(true);
-        dbf.setIgnoringElementContentWhitespace(true);
-        dbf.setIgnoringComments(true);
+        //dbf.setCoalescing(true);
+        //dbf.setIgnoringElementContentWhitespace(true);
+        //dbf.setIgnoringComments(true);
+        
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc1 = db.parse(expectedFile);
         doc1.normalizeDocument();
         Document doc2 = db.parse(realFile);
         doc2.normalizeDocument();
-        Assert.assertTrue(doc1.isEqualNode(doc2));
+        
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreComments(true);
+        XMLUnit.setIgnoreAttributeOrder(true);
+        XMLUnit.setNormalize(true);
+
+        
+        Diff diff = new Diff(doc1, doc2);
+        DetailedDiff ddiff = new DetailedDiff(diff);
+        List list = ddiff.getAllDifferences();
+        boolean equal = true;
+        for (Object le : list) {
+            Difference d = (Difference) le;
+            // ignore differences in namespace prefix names
+            if (!d.equals(DifferenceEngine.NAMESPACE_PREFIX)) {
+                equal = false;
+                break;
+            }
+        }
+        Assert.assertTrue("xml not identical " + diff, equal);
+        //Assert.assertTrue("xml not similar " + myDiff, myDiff.similar());
     }
 
     public static void write(Object jaxbAnnotatedObj, OutputStream os) throws JAXBException {
