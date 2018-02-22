b33ab2f1aaf37fe4d148edc7abe4aed6b8d22786
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2018-02-22 10:06:49 +0100

    added any attribute and ordering option and test for DiscourseConnection

--- src/main/java/eu/clarin/weblicht/wlfxb/tc/api/DiscourseConnective.java
+++ src/main/java/eu/clarin/weblicht/wlfxb/tc/api/DiscourseConnective.java
@@ -27,7 +27,7 @@ package eu.clarin.weblicht.wlfxb.tc.api;
  * @author Yana Panchenko
  *
  */
-public interface DiscourseConnective {
+public interface DiscourseConnective extends AnyAtrributes{
 
     public String getType();
 }
