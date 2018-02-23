3588d08abe6d26de9d8d25303d11fc164bda2020
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2018-02-23 11:21:13 +0100

    added extra attributes and ordering Reference layer

--- src/test/java/eu/clarin/weblicht/wlfxb/tclayers/test/ReferencesTest.java
+++ src/test/java/eu/clarin/weblicht/wlfxb/tclayers/test/ReferencesTest.java
@@ -21,6 +21,7 @@ import org.junit.rules.TemporaryFolder;
 public class ReferencesTest {
 
     private static final String INPUT = "/data/tc-refs/layer-input.xml";
+    private static final String INPUT_ANY_ATTRIBUTES = "/data/tc-refs/layer-inputAnyAtt.xml";
 
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
@@ -30,7 +31,6 @@ public class ReferencesTest {
 
         InputStream is = this.getClass().getResourceAsStream(INPUT);
         OutputStream os = new FileOutputStream(testFolder.newFile("layer-output.xml"));
-
 
         ReferencesLayer layer = TestUtils.read(ReferencesLayerStored.class, is);
         System.out.println(layer);
@@ -48,5 +48,45 @@ public class ReferencesTest {
         Assert.assertEquals(2, layer.getReferencedEntity(0).getReferences().length);
         Assert.assertEquals("pro.per3", layer.getReferencedEntity(0).getReferences()[0].getType());
         Assert.assertEquals("cataphoric", layer.getReferencedEntity(0).getReferences()[0].getRelation());
+    }
+
+    @Test
+    public void testReadAndWriteBackAnyAttributes() throws Exception {
+
+        InputStream is = this.getClass().getResourceAsStream(INPUT_ANY_ATTRIBUTES);
+        OutputStream os = new FileOutputStream(testFolder.newFile("layer-output.xml"));
+
+        ReferencesLayer layer = TestUtils.read(ReferencesLayerStored.class, is);
+        System.out.println(layer);
+        TestUtils.write(layer, os);
+
+        is.close();
+        os.close();
+
+        Assert.assertEquals("BART", layer.getTypetagset());
+        Assert.assertEquals("TuebaDZ", layer.getReltagset());
+        Assert.assertEquals(true, layer.hasExternalReferences());
+        Assert.assertEquals("Wikipedia", layer.getExternalReferenceSource());
+        Assert.assertEquals(2, layer.size());
+        Assert.assertEquals("http://de.wikipedia.org/wiki/Wahre_Finnen", layer.getReferencedEntity(0).getExternalId());
+        Assert.assertEquals(2, layer.getReferencedEntity(0).getReferences().length);
+        Assert.assertEquals("pro.per3", layer.getReferencedEntity(0).getReferences()[0].getType());
+        Assert.assertEquals("cataphoric", layer.getReferencedEntity(0).getReferences()[0].getRelation());
+
+        System.out.println(layer.getReferencedEntity(0).getReferences()[0].getExtraAtrributes());
+        Integer index = 0;
+        /*for (String anyAttribute : layer.getReferencedEntity(0).getReferences()[0].getExtraAtrributes().keySet()) {
+            if (index == 0) {
+                Assert.assertEquals("baseForm", anyAttribute);
+                Assert.assertEquals("baseFormWordRef", layer.getReferencedEntity(0).getReferences()[0].getExtraAtrributes().get(anyAttribute));
+            }
+            if (index == 1) {
+                Assert.assertEquals("alterForm", anyAttribute);
+                Assert.assertEquals("alterFormWordRef", layer.getReferencedEntity(0).getReferences()[0].getExtraAtrributes().get(anyAttribute));
+            }
+
+            index++;
+        }*/
+
     }
 }
