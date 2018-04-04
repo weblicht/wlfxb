67fc76d0fbaf3e7e3bad0399939af1cc2944faf5
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2018-04-04 15:51:59 +0200

    deleted unused imports from chunk layer

--- src/main/java/eu/clarin/weblicht/wlfxb/tc/xb/ChunkLayerStored.java
+++ src/main/java/eu/clarin/weblicht/wlfxb/tc/xb/ChunkLayerStored.java
@@ -35,15 +35,12 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.namespace.QName;
-
 /**
  *
  * @author felahi
@@ -85,9 +82,6 @@ public class ChunkLayerStored extends TextCorpusLayerStoredAbstract implements C
             for (String type : ch.getTypes().keySet()) {
                 foundTypes.add(type.toString());
             }
-            /*for (QName type : ch.getType().keySet()) {
-                foundTypes.add(type.toString());
-            }*/
         }
     }
 
@@ -146,7 +140,6 @@ public class ChunkLayerStored extends TextCorpusLayerStoredAbstract implements C
     @Override
     public Chunk addChunk(LinkedHashMap<String, String> chunkAttributes, List<Token> chunkTokens) {
         ChunkStored ch = new ChunkStored();
-        //ch.types = chunkAttributes;
         ch.attributes = ch.getAttributes(chunkAttributes);
         ch.tokRefs = new String[chunkTokens.size()];
         for (int i = 0; i < chunkTokens.size(); i++) {
