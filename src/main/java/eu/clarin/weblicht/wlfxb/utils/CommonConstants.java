ae67478e75f3b9577dff990482ab23034ef2140d
Emanuel Dima <emanueldima@gmail.com>
2020-01-30 14:47:35 +0100

    fix rng schema location for tcf04

--- src/main/java/eu/clarin/weblicht/wlfxb/utils/CommonConstants.java
+++ src/main/java/eu/clarin/weblicht/wlfxb/utils/CommonConstants.java
@@ -33,13 +33,12 @@ public class CommonConstants {
             "https://raw.githubusercontent.com/weblicht/tcf-spec/master/src/main/rnc-schema/d-spin_5.rnc",
             "application/relax-ng-compact-syntax");
     public static final String XML_WL1_MODEL_PI_CONTENT_FOR_VERSION_04 = String.format("href=\"%s\" type=\"%s\"",
-            "http://de.clarin.eu/images/weblicht-tutorials/resources/tcf-04/schemas/latest/d-spin_0_4.rnc",
+            "https://raw.githubusercontent.com/weblicht/tcf-spec/master/src/main/rnc-schema/d-spin_0_4.rnc",
             "application/relax-ng-compact-syntax");
     public static final String XML_MODEL_DECLARATION_WITH_WL1_PI_CONTENT = "\n<?xml-model "
             + "href=\"http://de.clarin.eu/images/weblicht-tutorials/resources/tcf-04/schemas/"
             + "latest/d-spin_0_4.rnc\" "
             + "type=\"application/relax-ng-compact-syntax\"?>\n";
-    
+
     public static final String CMD_SCHEMA_LOCATION = "http://www.clarin.eu/cmd/ http://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/profiles/clarin.eu:cr1:p_1320657629623/xsd";
-    
 }
